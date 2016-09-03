944e24d67ef0368994871ba07716dc7df199fdcc
Alex Daly <adalyuf@gmail.com>
2016-09-03 15:06:47 -0400

    Working charts

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -20,6 +20,7 @@ class DatasetsController < ApplicationController
   def show
     @dataset = Dataset.find_by(internal_name: params[:internal_name])
     @indicators = Indicator.where( dataset_id: @dataset.id )
+    @category = Category.find(@dataset.category_id)
   end
 
   def delete
--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -18,7 +18,21 @@ class IndicatorsController < ApplicationController
   def show
     @dataset = Dataset.find_by(internal_name: params[:dataset_internal_name])
     @indicator = Indicator.find_by(dataset_id: @dataset.id, internal_name: params[:internal_name])
-    @series = Series.where( indicator_id: @indicator.id )
+    @category = Category.find(@dataset.category_id)
+    @series = Series.where(indicator_id: @indicator.id)
+    @values = Value.where(indicator_id: @indicator.id)
+
+    grouped_values = @values.group_by(&:series_id)
+
+    @data = @series.map do |serie|
+      {
+        :name => serie.name,
+        :data => Hash[grouped_values[serie.id].map{ |value| [value.date, value.value] }]
+      }
+    end
+
+
+
   end
 
   def delete
