a11b3f40cf5bb21a66bab6a9e521525d53a5b631
Alex Daly <adalyuf@gmail.com>
2016-09-21 21:28:44 -0400

    Add searching for indicators based on whether they have a series with that attribute

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -1,16 +1,34 @@
 class IndicatorsController < ApplicationController
 
   def index
-    start = Time.now
     @indicators = Indicator.search(search_params[:query])
-    Rails.logger.error("Time to search for #{search_params[:query]}: #{ Time.now - start }")
+
+    @geo_codes = GeoCode.all
+    @frequencies = Frequency.all
+    @units = Unit.all
+    @genders = Gender.all
+    @races = Race.all
+    @age_brackets = AgeBracket.all
+    @employment_statuses = EmploymentStatus.all
+    @education_levels = EducationLevel.all
+    @child_statuses = ChildStatus.all
+    @income_levels = IncomeLevel.all
+    @occupation_codes = OccupationCode.all
+    @industry_codes = IndustryCode.all
+
+    @filters = filters.reject { |_,f| f.blank? }
+    if @filters.any?
+      @indicators = Indicator.where(id: Series.select(:indicator_id).where(@filters))
+    else
+      @indicators = Indicator.search(search_params[:query])
+    end
   end
 
   def show
-    start = Time.now
     @dataset = Dataset.find_by(internal_name: params[:dataset_internal_name])
     @indicator = Indicator.find_by(dataset_id: @dataset.id, internal_name: params[:internal_name])
     @category = Category.find(@dataset.category_id)
+
     @geo_codes = GeoCode.all
     @frequencies = Frequency.all
     @units = Unit.all #Unit is always the same for a given indicator, that is how we have defined it...
@@ -48,8 +66,6 @@ class IndicatorsController < ApplicationController
         }
       end
     end
-
-    Rails.logger.info("time to render show: #{ Time.now - start }")
   end
 
 
